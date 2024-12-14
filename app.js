9b731d79add68064dbed4829e1cc94be2a9d9fa5
Jose Daniel <jcastro07nuevo>
2024-12-13 23:42:14 -0400

    Introduccion a la consola

--- app.js
+++ app.js
@@ -7,9 +7,44 @@
 // let d = 10;
 // let x = a + b;
 
+// console.log('El resultado es: ' + x );
+
+// let a = 10,
+//     b = 20,
+//     c = 30,
+//     x = a + b;
+
+// console.log('a-'+a ); aqui concatenamos
+// console.log('b-'+b );
+// console.log('c-'+c );
+// console.log('d-'+d );
+
+
+// let a = 10,
+//     b = 20,
+//     c = 30,
+//     d = 40,
+//     x = a + b;
+
+// console.log({ a }); //De esta manera podemos mostrar como objetos
+// console.log({ b });
+// console.log({ c });
+// console.log({ d });
+
+// let a = 10,
+//     b = 20,
+//     c = 'Hola',
+//     d = 'Spiderman',
+//     x = a + b;
+    
+// console.table([a, b, c, d, x]);
+
 let a = 10,
     b = 20,
-    c = 30,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
 
-console.log("El resultado es: " + x );
+const saludo =  c + d;
+
+// console.log(saludo);
