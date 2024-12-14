15426a665d2c0ead0028ad3409e0d892c3cd71a8
Jose Daniel <jcastro07nuevo>
2024-12-13 23:17:56 -0400

    First Commit-Fundamentos

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// console.log('Hola Mundo');
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a + b;
+
+let a = 10,
+    b = 20,
+    c = 30,
+    x = a + b;
+
+console.log("El resultado es: " + x );
