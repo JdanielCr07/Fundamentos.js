7bc0a63cf14050f21b1a4936bf589d201252c921
Jose Daniel <jcastro07nuevo>
2024-12-14 23:13:13 -0400

    Agregamos Assets

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+
+
+
+// alert('Hola desde app.js');
+
+
+// console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo =  c + d;
+
+// console.log({ a }); //De esta manera podemos mostrar como objetos
+// console.log({ b });
+// console.log({ c });
+// console.log({ d });
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x});
+
+let miNuevonombre = 'Jose Daniel';
+
+
+
+
