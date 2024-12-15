931fc06e0bd5dd166c18bb30c70ce2a326034e88
Jose Daniel <jcastro07nuevo>
2024-12-15 11:21:32 -0400

    agregamos archivo arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,33 @@
+//ARREGLOS
+
+// const arr = new Array(10);
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = ['mario 3', 'megaman', 'chrono trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+console.log(videoJuegos[1]);
+console.log(videoJuegos[2]);
+
+let arregloCosas = [
+    true,
+    40,
+    'Jose Daniel',
+    1+2,
+    function(){},
+    ()=>{},
+    { a:1 },
+    ['x', 'megaman', 'zero', 'dr light',[
+        'dr willy',
+        'woodman'   
+    ]]
+
+];
+
+// console.log({arregloCosas});
+// console.log(arregloCosas[0]);
+// console.log(arregloCosas[2]);
+// console.log(arregloCosas[7][3]);
+console.log(arregloCosas[7][4][1]);
