f8e760b8ea00b71d0f17d89a2725f31ba00c6655
Jose Daniel <jcastro07nuevo>
2024-12-16 23:25:12 -0400

    actualizacion de funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,7 +1,8 @@
 //FUNCIONES
 
 function saludar( nombre) {
-    console.log('Hola ' + nombre);
+    // console.log('Hola ' + nombre);
+    return [1,2];
 }
 
 const saludar2 = function( nombre ){
@@ -13,6 +14,39 @@ const saludar3 = (nombre) => {
 }
 
 
-saludar('Dani');
-saludar2('Joseda');
-saludar3('Maria');
+const retornoSaludar = saludar('Dani');
+
+// saludar2('Joseda');
+// saludar3('Maria');
+
+function sumar (a,b){
+    return a + b;
+};
+
+console.log( sumar(40,2) );
+
+const sumar2 = (a,b) =>{
+    return a + b;
+};
+
+console.log( sumar2(20,30) );
+
+const sumar3 = (a,b) => a + b;//asi se puede hacer siempre y cuanto se quiere ejecutar el mismo return
+
+console.log( sumar3(60,30) );
+
+function getAleatorio(){
+    return Math.random();
+}
+
+console.log( getAleatorio() );
+
+const getAleatorio2 = () => {
+    return Math.random();
+}
+
+console.log( getAleatorio2() );
+
+const getAleatorio3 = () => Math.random();
+
+console.log( getAleatorio3() );
